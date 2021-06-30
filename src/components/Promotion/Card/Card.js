e8942f9264a5233ef162dad1dcdb5e2f4469a274
Abel Souza <abelsouzacosta@gmail.com>
2021-06-30 03:36:11 -0300

    Estilizando o componente

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Card.css";
+
 /**
  * recebe um objeto retornado da api e renderiza dentro do card da aplicação
  *
@@ -7,28 +9,38 @@ import React from "react";
  */
 const PromotionCard = ({ promotionApiResponse }) => {
   return (
-    <div>
+    <div className="promotion-card">
       <img
+        className="promotion-card__image"
         src={promotionApiResponse.imageUrl}
         alt={promotionApiResponse.title}
       />
-      <div>
-        <h1>{promotionApiResponse.title}</h1>
-        <span>R$ {promotionApiResponse.price}</span>
-        <footer>
+      <div className="promotion-card__info">
+        <h1 className="promotion-card__title">{promotionApiResponse.title}</h1>
+        <span className="promotion-card__price">
+          R$ {promotionApiResponse.price}
+        </span>
+        <footer className="promotion-card__footer">
           {promotionApiResponse.comments.length > 0 && (
             // verifica se há algum comentário e se houver vai
             // renderizar uma div que mostra o primeiro comentário escrito
-            <div>"{promotionApiResponse.comments[0].comment}"</div>
+            <div className="promotion-card__comment">
+              "{promotionApiResponse.comments[0].comment}"
+            </div>
           )}
 
-          <div>
+          <div className="promotion-card__comments-count">
             {promotionApiResponse.comments.length}{" "}
             {promotionApiResponse.comments.length > 1
               ? "Comentários"
               : "Comentário"}
           </div>
-          <a href={promotionApiResponse.url} target="_blank" rel="noreferrer">
+          <a
+            href={promotionApiResponse.url}
+            target="_blank"
+            rel="noreferrer"
+            className="promotion-card__link"
+          >
             IR PARA O SITE
           </a>
         </footer>
