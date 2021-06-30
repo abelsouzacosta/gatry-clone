35d9a3163d5eb726b9c54cb48ddeae2ae300d75b
Abel Souza <abelsouzacosta@gmail.com>
2021-06-30 02:24:39 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -3,30 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  // estado inicial da aplicação -> false
-  const [display, setDisplay] = useState(false);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={() => setDisplay((display) => !display)}>
-          Mostrar
-        </button>
-        {display && <img src={logo} className="App-logo" alt="logo" />}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
