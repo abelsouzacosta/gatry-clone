c33473595bc81a376b25d58626a1c3eb50f1085d
Abel Souza <abelsouzacosta@gmail.com>
2021-06-30 01:37:14 -0300

    Componente funcional

--- src/App.js
+++ src/App.js
@@ -2,46 +2,26 @@ import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showImage: false,
-    };
-  }
-
-  toggle = () => {
-    this.setState({
-      showImage: !this.state.showImage,
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button onClick={this.toggle}>
-            {this.state.showImage ? "Esconder" : "Mostrar"}
-          </button>
-          {this.state.showImage && (
-            <img src={logo} className="App-logo" alt="logo" />
-          )}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>Mostrar</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
