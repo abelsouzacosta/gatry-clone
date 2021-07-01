eb62478b2d5f2b43f55b5cd1ac681a1f07711191
Abel Souza <abelsouzacosta@gmail.com>
2021-07-01 05:09:06 -0300

    Comunicação com a api implementada

--- src/pages/Promotion/Search/Search.js
+++ src/pages/Promotion/Search/Search.js
@@ -9,38 +9,19 @@ import PromotionCard from "components/Promotion/Card/Card";
 import axios from "axios";
 
 const PagesPromotionSearch = () => {
-  const [promotionList, setPromotionList] = useState([]);
+  const [promotions, setPromotions] = useState([]);
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/promotions?_embed=comments")
-      .then((response) => setPromotionList(response.data));
+      .then((response) => setPromotions(response.data));
   }, []);
-
-  const promotionApiReturnResponse = {
-    id: 1,
-    title:
-      "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
-    price: 1799,
-    imageUrl:
-      "https://cdn.gatry.com/gatry-static/promocao/imagem_url/2631517face1861bc4f46ae154d387de.png",
-    url: "https://www.amazon.com.br/Notebook-Acer-Mochila-A315-41-R790-Mem%C3%B3ria/dp/B07YDWLV7S/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=gatry0b-20&linkId=e4a1146599e36741a720a6a952cbc328&language=pt_BR",
-    comments: [
-      {
-        id: 1,
-        comment: "TELA HD",
-      },
-    ],
-  };
 
   return (
     <div className="main">
-      <PromotionCard promotionApiResponse={promotionApiReturnResponse} />
-      {promotionList.length > 0 &&
-        promotionList.map((promotionApiReturnResponse) => {
-          return (
-            <PromotionCard promotionApiResponse={promotionApiReturnResponse} />
-          );
+      {promotions.length > 0 &&
+        promotions.map((promotion) => {
+          return <PromotionCard promotionApiResponse={promotion} />;
         })}
     </div>
   );
