24a1861edddef89185a99d67b2e564ca2ab1f83a
Abel Souza <abelsouzacosta@gmail.com>
2021-07-02 10:24:35 -0300

    Adicionando useApi hook

---
+++ src/components/utils/useApi.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+import axios from "axios";
+
+/**
+ * valores iniciais do request
+ *
+ * Esses valores foram isolados para se no caso de
+ * se usar o hook para fazer mais de uma requisição ao mesmo
+ * tempo teriamos um requestInfo com inforamções erradas, onde ao
+ * mesmo tempo que há erro, há também informações
+ *
+ * Dessa forma podemos atualizar o estado com base no
+ * objeto inicial
+ */
+const initialRequestInfo = {
+  error: null,
+  data: null,
+  loading: false,
+};
+
+/**
+ * recebe um objeto de confirguração e retorna o que
+ * é a resposta da api
+ * @param {Object} config - objeto de configuração do axios
+ * @returns
+ */
+export default function useApi(config) {
+  const [requestInfo, setRequestInfo] = useState(initialRequestInfo);
+
+  async function call() {
+    setRequestInfo({
+      ...initialRequestInfo,
+      loading: true,
+    });
+
+    const response = await axios(config);
+
+    setRequestInfo({
+      ...initialRequestInfo,
+      data: response.data,
+    });
+
+    if (config.onCompleted) {
+      config.onCompleted(response);
+    }
+  }
+
+  return [call, requestInfo];
+}
