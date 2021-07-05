d3af9180ea5bd87d17446f5cb2fe4cce3b7fe02b
Abel Souza <abelsouzacosta@gmail.com>
2021-07-05 09:13:27 -0300

    nenhum comentário foi encontrado para a promoção

--- src/components/Promotion/Modal/CommentsTree/CommentsTree.jsx
+++ src/components/Promotion/Modal/CommentsTree/CommentsTree.jsx
@@ -5,6 +5,9 @@ import "./CommentsTree.css";
 const PromotionModalCommentsTree = ({ comments }) => {
   if (!comments) return <div>Carregando...</div>;
 
+  if (comments.length === 0)
+    return <div>Nenhum comentário foi encontrado para essa promoção</div>;
+
   return (
     <ul className="promotion-modal-comments-tree">
       {comments.map((item) => {
