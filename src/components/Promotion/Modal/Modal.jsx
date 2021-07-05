9b8a9bd22b7dfa2076ee9f00092c00851016c93d
Abel Souza <abelsouzacosta@gmail.com>
2021-07-05 08:27:16 -0300

    Renderizando o componente modal

--- src/components/Promotion/Modal/Modal.jsx
+++ src/components/Promotion/Modal/Modal.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import UIModal from "components/UI/Modal/Modal";
 
+import useApi from "components/utils/useApi";
+
 const PromotionModal = ({ promotionId, onClickClose }) => {
+  const [load, loadInfo] = useApi({
+    url: `/comments`,
+    params: {
+      promotionId,
+    },
+  });
+
+  useEffect(() => {
+    load();
+    console.log(loadInfo);
+  }, []);
+
   return (
     <UIModal isOpen={Boolean(promotionId)} onClickClose={onClickClose}>
       <h1>Comentários no modal</h1>
